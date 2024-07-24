9138eb89ab69bee2320ca193cbd24ede75cffa2b
Harvey Yerik <yerik05vh@gmail.com>
2024-07-23 23:33:36 -0500

    :sparkles: Add some

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <></>;
+  return <>
+    <h2>Hola mundo</h2>
+    <div>Hola mundo</div>
+    <div></div>
+    <input type="text" />
+    <div className="Esto un mensaje normal y cualquiera">
+      Locura
+    </div>
+  </>;
 }
 
 export default App;
