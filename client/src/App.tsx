28b30ccffdb023c7b6c255aca17ad7758d87903c
Harvey Yerik <yerik05vh@gmail.com>
2024-07-24 23:12:43 -0500

    :fire: Remove some code line

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,5 @@
 function App() {
   return <>
-    <h2>Hola mundo</h2>
-    <div>Hola mundo</div>
-    <div></div>
-    <input type="text" />
-    <div className="Esto un mensaje normal y cualquiera">
-      Locura
-    </div>
   </>;
 }
 
