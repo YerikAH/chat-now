9971a582e2fdf552b009dfa450ec92a02030fb19
Harvey Yerik <yerik05vh@gmail.com>
2024-07-31 11:23:54 -0500

    :sparkles: Add config client project.

---
+++ client/src/context/theme/ThemeContext.tsx
@@ -0,0 +1,19 @@
+import {Theme} from '@/types';
+import {createContext, useContext} from 'react';
+
+interface ThemeContextProps {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const useTheme = (): ThemeContextProps => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
+
+export default ThemeContext;
